26dff9cf3c507b4602055fddc6017f5e3b2d97f9
mausam gurung <118035408+ShahGar@users.noreply.github.com>
2024-05-04 10:01:32 +0545

    authentication

---
+++ src/main/urls.py
@@ -0,0 +1,10 @@
+
+from django.conf import settings
+from django.urls import path
+
+from .views import home_view, main_view
+
+urlpatterns = [
+    path('', main_view, name='main'),
+    path('home/', home_view, name='home'),
+]
